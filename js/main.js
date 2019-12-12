f16ee92f6e374dcaffa3021c09382660f7aa94d1
Fenris-c <anatolijv236@gmail.com>
2019-12-12 20:21:55 +0300

    small fixes

--- js/main.js
+++ js/main.js
@@ -15,8 +15,8 @@ $(document).ready(function () {
         });
     }
 
-    $parallaxBox.mousemove(function(e) {
-        if(window.innerWidth > 1000) {
+    $parallaxBox.mousemove(function (e) {
+        if (window.innerWidth > 1000) {
             parallaxIt(e, '.cubeParallax__second', 45, 0);
             parallaxIt(e, '.cubeParallax__third', -50, 50);
         }
@@ -41,4 +41,22 @@ $(document).ready(function () {
         }
         return false;
     });
+
+    /**
+     * TAGS
+     */
+    $('.tags__content').fadeOut(0);
+    $('.tags__content_active').fadeIn(0);
+
+
+    $('.tags__item').on('click', function () {
+        $('.tags__item').removeClass("active");
+        $('.tags__content').fadeOut(500);
+        // $('.tags__content').removeClass("tags__content_active");
+        // $('.tags__content').eq($(this).index()).addClass("tags__content_active");
+        $('.tags__content').eq($(this).index()).delay(500).fadeIn(500);
+        $(this).addClass("active");
+    })
+
+
 });
